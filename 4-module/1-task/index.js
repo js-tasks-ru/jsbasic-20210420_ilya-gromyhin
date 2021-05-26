3ad9717a2907bd2e88bdc167d6c9326a68d9cccc
itgrom <itgrom@mail.ru>
2021-05-26 23:57:58 +0300

    Решение задач в модуле: 4-module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+    let ul = document.createElement('ul');
+
+    for (el of friends) {
+        let li = document.createElement('li');
+        li.textContent = el['firstName'] + ' ' + el['lastName'];
+        ul.append(li);
+    }
+    return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,35 @@
 function highlight(table) {
-  // ваш код...
+    
+    let headRows = table.rows[0];
+    let columnsIndex = {};
+
+    console.log(headRows);
+    for (let i = 0; i < headRows.cells.length; i++) {
+        columnsIndex[headRows.cells[i].textContent] = i;
+    }
+
+    for (let i = 1; i < table.rows.length; i++) {
+        let tdNew = document.createElement('td');
+        let row = table.rows[i];
+        let name = row.cells[columnsIndex.Name];
+        let age = row.cells[columnsIndex.Age];
+        let gender = row.cells[columnsIndex.Gender];
+        let status = row.cells[columnsIndex.Status];
+
+        for (cell of row.cells) {
+            if (cell.dataset.available) {
+                cell.hidden = true;
+            }
+        }
+
+        status.dataset.available == true ? status.classList.add('available') : status.classList.add('unavailable');
+        console.log(status);
+
+        gender.textContent == 'm' ? gender.classList.add('male') : gender.classList.add('female');
+        console.log(gender);
+
+        +
+        (age.textContent) < 18 ? age.style.textDecoration = 'line-through' : age.style.textDecoration = '';
+        console.log(age);
+    }
 }
