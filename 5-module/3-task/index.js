324249232b13f89722b1a8ac108967e6b335ac52
itgrom <itgrom@mail.ru>
2021-05-28 11:10:31 +0300

    Добавил решение для модуля 5-module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
 function initCarousel() {
-  // ваш код...
+  let leftButton = document.querySelector('.carousel__arrow_left');
+  let rightButton = document.querySelector('.carousel__arrow_right');
+  let inner = document.querySelector('.carousel__inner');
+  let innerWidth = inner.offsetWidth;
+  let currentOffset = 0;
+  let currentSlaid = 1;
+  currentSlaid == 1 ? leftButton.style.display = 'none' : leftButton.style.display = '';
+  
+  rightButton.onclick = function () {
+    currentSlaid += 1;
+    currentSlaid == 1 ? leftButton.style.display = 'none' : leftButton.style.display = '';
+    currentSlaid == 4 ? this.style.display = 'none' : this.style.display = '';
+  
+    inner.style.transform = `translateX(${currentOffset - innerWidth}px)`;
+    currentOffset -=innerWidth;
+  
+}
+leftButton.onclick = function () {
+  currentSlaid -= 1
+  currentSlaid == 1 ? this.style.display = 'none' : this.style.display = '';
+  currentSlaid == 4 ? rightButton.style.display = 'none' : rightButton.style.display = '';
+  inner.style.transform = `translateX(${currentOffset + innerWidth}px)`;
+  currentOffset +=innerWidth;
+  
+}
+
+
+
 }
